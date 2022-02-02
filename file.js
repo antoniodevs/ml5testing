10602100c68f2af6af399477ce199e8a0f9d7579
Antonio Carlos <antoniocarlos.devs@gmail.com>
2022-02-02 15:06:29 -0300

    Adicao de validacao de formulario para enviar imagem e funcao para ocultar form apos envio da imagem

--- file.js
+++ file.js
@@ -1,22 +1,37 @@
 const form = document.getElementById("form-img");
 var exibeImagem = document.getElementById("img-selected");
 const classificar = document.getElementById("btn-classificar");
-var pathimagem;
+var pathImagem;
 var imageClassifier;
+var formUpload = true;
+var btnClassificar = true;
 
 form.addEventListener("submit", (event) => {
-  pathImagem = form.elements["img-upload"].value;
   event.preventDefault();
+  pathImagem = form.elements["img-upload"].value;
+
   nomeImagem = extrairDadosArquivo(pathImagem);
   imageClassifier =
     "<img src='images/" + nomeImagem + "' id='image' width='400'/>";
   exibeImagem.innerHTML = imageClassifier;
 
-  classificar.style.display = "inline";
+  // classificar.style.display = "inline";
+  displayElement(classificar, btnClassificar);
+  displayElement(form, !formUpload);
+
+  console.log("pathImagem: ", pathImagem);
 });
 
 function extrairDadosArquivo(path) {
   var split = path.split("\\");
   var arquivo = split[split.length - 1];
   return arquivo;
+}
+
+function displayElement(element, display) {
+  if (display) {
+    element.style.display = "inline";
+  } else {
+    element.style.display = "none";
+  }
 }
