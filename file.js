ea5b96a35a78d1a726b37ac305459f58f41cbcca
Antonio Carlos <antoniocarlos.devs@gmail.com>
2022-02-02 16:18:14 -0300

    Adicao de opcao para trocar imagem e fazer novo upload

--- file.js
+++ file.js
@@ -1,10 +1,13 @@
 const form = document.getElementById("form-img");
 var exibeImagem = document.getElementById("img-selected");
 const classificar = document.getElementById("btn-classificar");
+const btnUploadImage = document.getElementById("btn-reload");
 var pathImagem;
 var imageClassifier;
 var formUpload = true;
 var btnClassificar = true;
+var uploadImage = true;
+var imgUpload = true;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -16,16 +19,24 @@ form.addEventListener("submit", (event) => {
   exibeImagem.innerHTML = imageClassifier;
 
   // classificar.style.display = "inline";
+  displayElement(exibeImagem, imgUpload);
   displayElement(classificar, btnClassificar);
+  displayElement(btnUploadImage, uploadImage);
   displayElement(form, !formUpload);
-
-  console.log("pathImagem: ", pathImagem);
 });
 
 function extrairDadosArquivo(path) {
   var split = path.split("\\");
   var arquivo = split[split.length - 1];
   return arquivo;
+}
+
+function trocaImagem() {
+  form.elements["img-upload"].value = "";
+  displayElement(exibeImagem, !imgUpload);
+  displayElement(form, formUpload);
+  displayElement(classificar, !btnClassificar);
+  displayElement(btnUploadImage, !uploadImage);
 }
 
 function displayElement(element, display) {
